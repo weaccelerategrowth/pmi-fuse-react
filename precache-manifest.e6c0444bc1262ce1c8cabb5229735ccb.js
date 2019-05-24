60a88ca46e48ce3b4ccf4c71cd89dc3ac1c879bb
Joseph Izatt <joe@izatt.co.uk>
2019-05-24 14:49:51 +0100

    Updates

---
+++ precache-manifest.e6c0444bc1262ce1c8cabb5229735ccb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c67069eed33807fef409a1d19fdf5721",
+    "url": "/pmi-fuse-react/static/media/pmi-logo@2x.c67069ee.png"
+  },
+  {
+    "revision": "53b2620472b66178118dfac6387854ed",
+    "url": "/pmi-fuse-react/static/media/pmi-logo-large@2x.53b26204.png"
+  },
+  {
+    "revision": "ed88977fb3a7ee14a651a6d7173b1f4d",
+    "url": "/pmi-fuse-react/static/media/fuse-pmi-logo@2x.ed88977f.png"
+  },
+  {
+    "revision": "4f04c4a8030b041f0b3e",
+    "url": "/pmi-fuse-react/static/js/vendor.min.js"
+  },
+  {
+    "revision": "e270f7827791ab705578",
+    "url": "/pmi-fuse-react/static/js/main.min.js"
+  },
+  {
+    "revision": "e270f7827791ab705578",
+    "url": "/pmi-fuse-react/static/css/main.css"
+  },
+  {
+    "revision": "4f04c4a8030b041f0b3e",
+    "url": "/pmi-fuse-react/static/css/1.a7b5d079.chunk.css"
+  },
+  {
+    "revision": "a425f5991d5f4a03d5b2bf692a38948e",
+    "url": "/pmi-fuse-react/index.html"
+  }
+];
